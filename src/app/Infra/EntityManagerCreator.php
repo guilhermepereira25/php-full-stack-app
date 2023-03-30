4519a91b10b5642e7e8f818f4b4234170b53a43a
guilherme-ubu <guilhermepereiradv@gmail.com>
2023-03-29 22:50:27 -0300

    commit db config

--- src/app/Infra/EntityManagerCreator.php
+++ src/app/Infra/EntityManagerCreator.php
@@ -2,21 +2,40 @@
 
 namespace Application\Source\Infra;
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\MissingMappingDriverImplementation;
+use Doctrine\ORM\ORMSetup;
+
 class EntityManagerCreator
 {
-    public function getEntityManager()
+    /**
+     * @throws MissingMappingDriverImplementation
+     */
+    public function getEntityManager(): EntityManager
     {
         $paths = [__DIR__ . '/../Entity'];
-        $isDevMode = false;
 
-        //using docker params
+        //using docker params in .env
         $dbParams = [
             'host' => $_ENV['DB_HOST'],
             'user' => $_ENV['DB_USERNAME'],
             'password' => '',
-            'dbname' => 'product',
+            'dbname' => $_ENV['DB_DATABASE'],
             'charset' => 'UTF8',
             'driver' => 'pdo_mysql'
         ];
+
+        try {
+            $config = ORMSetup::createAttributeMetadataConfiguration($paths);
+            $connection = DriverManager::getConnection($dbParams, $config);
+        } catch (\Throwable $th) {
+            echo json_encode([
+                'message' => $th->getMessage(),
+                'code' => $th->getCode()
+            ]);
+        }
+
+        return new EntityManager($connection, $config);
     }
 }
