9262d40ed2b59493ed95166bd87450cc23885cd0
dev-guilherme <guilhermepereiradv@gmail.com>
2023-04-04 13:59:30 -0300

    react initial setup

---
+++ frontend/src/pages/Index/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Index() {
+    return (
+        <>
+            <h1>Index Page</h1>
+            <p>Welcome to the Index Page!</p>
+        </>
+    );
+}
